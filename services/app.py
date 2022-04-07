0d009621e449c3b94d63b75118f98fe6032e1c8e
lida24 <lidalenkova204@gmail.com>
2022-04-07 19:41:47 +0300

    added colors

--- services/app.py
+++ services/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from helper_functions import load_json_temp_log
+from helper_functions import load_json_temp_log, set_color
 
 app = Flask(__name__)
 
@@ -9,6 +9,6 @@ temp_log = load_json_temp_log()
 @app.route('/')
 def temp_page():
 
-    return render_template('temp.html', temp_log=temp_log)
+    return render_template('temp.html', temp_log=temp_log, set_color=set_color)
 
 app.run(debug=True)
