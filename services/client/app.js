334079f4f7dc9af6372f6a12987ed37ceb32f01b
lida24 <lidalenkova204@gmail.com>
2022-04-08 18:12:41 +0300

    client-server-api-flask

---
+++ services/client/app.js
@@ -0,0 +1,18 @@
+var url = "http://127.0.0.1:5000/get_heatmap";
+
+ fetch(url)
+ .then((resp) =>resp.json())
+ .then(function(data) {
+    for(let j = 0; j < data.heatmap.length; j++) {
+        var tr = document.createElement("tr");
+        for(let n = data.heatmap.length - 1; n >= 0 ; n--) {
+            var th = document.createElement("th");
+            th.textContent = data.heatmap[j][n].toString();
+            tr.appendChild(th);
+        }
+        document.getElementById('tbody').appendChild(tr);
+    }
+ })
+ .catch(function(error) {
+     console.log(error)
+ });
